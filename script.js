2da1b344e99a50557ed24a7632552b856ca27bd7
marwaneltary <marwaneltary@gmail.com>
2024-09-15 19:22:21 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log("welcome");
+console.log(7);
+console.log("my name is marwan");//this is js code
+var name="marwan"; //"string"
+console.log(name);
+var age=10 //number
+console.log(age);
+var email="marwan@gmail.com";//string
+console.log(email);
+var username=prompt("enter your name");
+console.log("welcome",username); //string
+console.log(typeof name);
+console.log(typeof age);
+var ishappy =true;//boolean
+console.log(ishappy);
+console.log(typeof ishappy);
+var issad =false; //boolean
+console.log(issad);
+console.log(typeof issad);
